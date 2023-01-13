50e6baa696f3f9b21614edc84ef0ebf7fb36607d
N P <nicolas.palaorocort03@gmail.com>
2023-01-13 09:40:49 +0100

    Reset + Login
    Borrado todo y empezado desde cero, login funcional

--- app/src/main/java/com/example/gestionhotel/MainActivity.java
+++ app/src/main/java/com/example/gestionhotel/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.gestionhotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText uName=(EditText) findViewById(R.id.uNameET);
+        EditText passwd=(EditText) findViewById(R.id.pwdET);
+
+        MaterialButton loginBtn=(MaterialButton) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (uName.getText().toString().equals("admin") && passwd.getText().toString().equals("admin"))
+                    Toast.makeText(MainActivity.this,"Login Correcto",Toast.LENGTH_LONG).show();
+                else Toast.makeText(MainActivity.this,"Login Incorrecto",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
